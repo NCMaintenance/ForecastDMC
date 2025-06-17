90f3b4845c6a5e7685823849fdae148b4ed7c84c
NCMaintenance <149673546+NCMaintenance@users.noreply.github.com>
2025-06-18 00:02:35 +0100

    Update DMC.py

--- DMC.py
+++ DMC.py
@@ -520,7 +520,7 @@ if uploaded_file:
 
                     # Initialize LightGBM Regressor with hyperparameters for better accuracy
                     model = lgb.LGBMRegressor(
-                        n_estimators=min(800, len(X) * 2), # Increased n_estimators, adjusted based on data size
+                        n_estimators=min(600, len(X) * 2), # Increased n_estimators, adjusted based on data size
                         learning_rate=0.1, # Further reduced learning rate for potentially higher accuracy
                         max_depth=min(4, len(available_features) + 1), # Increased max_depth, consider feature count
                         num_leaves=min(40, 2 ** min(10, len(available_features) + 1) - 1), # Increased num_leaves
